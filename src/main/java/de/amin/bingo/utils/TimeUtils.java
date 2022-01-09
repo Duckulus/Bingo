17ab2ecd62e8cc4f297d8813a9c5c0374acd9c2e
honigbrai <67374196+honigbrai@users.noreply.github.com>
2022-01-09 17:37:39 +0100

    Added hour fromat
    Added hour fromat to increase readability of the time

--- src/main/java/de/amin/bingo/utils/TimeUtils.java
+++ src/main/java/de/amin/bingo/utils/TimeUtils.java
@@ -4,18 +4,22 @@ public class TimeUtils {
 
     public static String formatTime(int seconds){
         if(seconds <= 0)return "";
-        int m = seconds / 60;
+        int h = seconds / 3600;
+        int m = (seconds / 60)%60;
         int s = seconds % 60;
+        String formattedH;
         String formattedM;
         String formattedS;
-        if(m>99){
-            formattedM = String.format("%03d", m);
-        }else {
-            formattedM = String.format("%02d", m);
-        }
-        formattedS = String.format("%02d", s);
 
-        return formattedM+":"+formattedS;
+        formattedM = String.format("%02d", m);
+        formattedS = String.format("%02d", s);
+        if(h>0){
+            formattedH = String.format("%02d", h);
+            return formattedH + ":" +formattedM+":"+formattedS;
+        }else {
+            formattedH = null;
+            return formattedM+":"+formattedS;
+        }
     }
 
 }
