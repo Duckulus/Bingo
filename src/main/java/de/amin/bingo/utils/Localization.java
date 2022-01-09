f0840f22c3d7ec1560a64593b4658bd254244232
Duckulus <duckelite1@gmail.com>
2022-01-09 01:19:53 +0100

    improved the way the language bundles are loaded from the jar

--- src/main/java/de/amin/bingo/utils/Localization.java
+++ src/main/java/de/amin/bingo/utils/Localization.java
@@ -8,20 +8,35 @@ import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Localization {
 
     private static HashMap<String, YamlConfiguration> localizations = new HashMap<>();
-
+    private static BingoPlugin plugin = BingoPlugin.INSTANCE;
 
     public static void load() {
-        BingoPlugin plugin = BingoPlugin.INSTANCE;
 
-        plugin.saveResource("localization/en_us.yml", false);
-        plugin.saveResource("localization/de_de.yml", false);
 
-        //register
+//        plugin.saveResource("localization/en_us.yml", false);
+//        plugin.saveResource("localization/de_de.yml", false);
+
+        //copy localization rescource bundle from jar into plugin data folder
+        try {
+            copyFromJar("localization/", Paths.get("plugins/bingo/localization/"));
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+
+        //register configurations for language bundle files
+        if(new File(plugin.getDataFolder(), "localization").listFiles()==null) {
+            return;
+        }
         for (File f : new File(plugin.getDataFolder(), "localization").listFiles()) {
             try {
                 YamlConfiguration config = new YamlConfiguration();
@@ -36,7 +51,7 @@ public class Localization {
     public static String get(Player player, String key, String... replacements) {
         //look for localization in players locale or fallback to english if not found
         YamlConfiguration config = localizations.getOrDefault(player.getLocale(), localizations.get("en_us"));
-        if (config.get(key) != null) {
+        if (config != null && config.get(key) != null) {
             String localizedMessage = ChatColor.translateAlternateColorCodes('&', config.getString(key));
             for (int i = 0; i < replacements.length; i++) {
                 localizedMessage = localizedMessage.replace("{" + i + "}", replacements[i]);
@@ -45,6 +60,43 @@ public class Localization {
         } else {
             return "MISSING " + player.getLocale() + ": " + key;
         }
+    }
+
+    public static void copyFromJar(String source, final Path target) throws URISyntaxException, IOException {
+        URI resource = BingoPlugin.class.getResource("").toURI();
+        try {
+            FileSystem fileSystem = FileSystems.newFileSystem(
+                    resource,
+                    Collections.<String, String>emptyMap()
+            );
+            final Path jarPath = fileSystem.getPath(source);
+
+            Files.walkFileTree(jarPath, new SimpleFileVisitor<Path>() {
+
+                private Path currentTarget;
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    currentTarget = target.resolve(jarPath.relativize(dir).toString());
+                    Files.createDirectories(currentTarget);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.copy(file, target.resolve(jarPath.relativize(file).toString()), StandardCopyOption.REPLACE_EXISTING);
+                    return FileVisitResult.CONTINUE;
+                }
+
+            });
+            plugin.getLogger().info("§aSuccesfully loaded Language bundles!");
+        } catch (FileSystemAlreadyExistsException e) {
+            plugin.getLogger().info("§cNot copying Language files because they already exist!");
+        }
+
+
+
+
     }
 
 }
